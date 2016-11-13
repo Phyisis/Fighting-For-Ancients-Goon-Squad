fe2a2ad30dfc2d902fabfd545f4644b687a3d29f
Phyisis <phyisisblue@gmail.com>
2016-11-13 02:15:53 -0500

    invoker int2

--- game/scripts/vscripts/heroes/invoker/ernest_int2.lua
+++ game/scripts/vscripts/heroes/invoker/ernest_int2.lua
@@ -1,24 +1,18 @@
 ernest_int2 = class({})
 
-function ernest_swap:OnSpellStart()
+function ernest_int2:OnSpellStart()
 	caster = self:GetCaster()
-	AddFOWViewer(caster:GetTeamNumber(), caster:GetCursorPosition(), self:GetAbility():GetSpecialValueFor("radius"), self:GetAbility():GetSpecialValueFor("duration"), false)
-	
+	AddFOWViewer(caster:GetTeamNumber(), caster:GetCursorPosition(), self:GetSpecialValueFor("radius"), self:GetSpecialValueFor("duration"), false)
 	
 	local damageTable = {
-		victim = FindUnitsInRadius(entity:GetTeam(), caster:GetCursorPosition(), nil, self:GetAbility():GetSpecialValueFor("radius"), DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, 0, 0, false ),
+		victim = FindUnitsInRadius(caster:GetOpposingTeamNumber(), caster:GetCursorPosition(), nil, self:GetSpecialValueFor("radius"), DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, 0, 0, false ),
 		attacker = caster,
-		damage = self:GetAbility():GetSpecialValueFor("flat_damage"),
+		damage = self:GetSpecialValueFor("flat_damage"),
 		damage_type = DAMAGE_TYPE_MAGICAL,
 	}
-
-	
+	print(victim)
 	Timers:CreateTimer(1, function()
 		ApplyDamage(damageTable)
 	end)
 	
 end
-
-
-FindUnitsInRadius(int teamNumber, Vector position, handle cacheUnit, float radius, int teamFilter, int typeFilter, int flagFilter, int order, bool canGrowCache)
-Finds the units in a given radius with the given flags. ( iTeamNumber, vPosition, hCacheUnit, flRadius, iTeamFilter, iTypeFilter, iFlagFilter, iOrder, bCanGrowCache )
